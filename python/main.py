2851e140d150aa24cd2a4a839a8c87f64f51604e
philippe86220 <142721115+philippe86220@users.noreply.github.com>
2026-04-30 11:02:47 +0200

    Add api_volume function to control volume

--- python/main.py
+++ python/main.py
@@ -4,6 +4,8 @@ import json
 from arduino.app_utils import App
 from arduino.app_bricks.web_ui import WebUI
 
+import requests
+
 ui = WebUI()
 
 HOST_SERVICE = "http://172.17.0.1:9000"
@@ -24,6 +26,18 @@ def api_stop():
 def api_status():
     return proxy_get("/status")
 
+def api_volume(value: int):
+    value = int(value)
+
+    if value < 0:
+        value = 0
+    if value > 100:
+        value = 100
+
+    r = requests.get(f"{HOST_SERVICE}/volume?value={value}", timeout=2)
+    return r.json()
+
+
 # routes
 ui.expose_api("GET", "/api/info", lambda: api_radio("info"))
 ui.expose_api("GET", "/api/rtl", lambda: api_radio("rtl"))
@@ -31,7 +45,7 @@ ui.expose_api("GET", "/api/inter", lambda: api_radio("inter"))
 ui.expose_api("GET", "/api/musique", lambda: api_radio("musique"))
 ui.expose_api("GET", "/api/nostalgie", lambda: api_radio("nostalgie"))
 ui.expose_api("GET", "/api/mradio", lambda: api_radio("mradio"))
-
+ui.expose_api("GET", "/api/volume", api_volume)
 ui.expose_api("GET", "/api/stop", api_stop)
 ui.expose_api("GET", "/api/status", api_status)
 
